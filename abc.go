e3ef6cc21e10af33593f0cdedfa3652c94a0ebfd
phpfrid <824431999@qq.com>
2017-03-03 15:32:47 +0800

    第一次提交

---
+++ abc.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+ Persion struct{
+	name stirng,
+	pwd string,
+	ape int
+}
+
+var a, b = 25, "abc"
+
+func test(a int, b int) {
+	if a > b {
+		fmt.Println(a)
+	} else {
+		fmt.Println(b)
+	}
+	fmt.Println(a + b)
+}
+
+func strtest(s string) {
+	c := []byte(s)
+	c[0] = 'd'
+	f := string(c)
+	fmt.Println(f)
+}
+
+func errtest() {
+	err := errors.New("有错误")
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = errors.New("emit macho dwarf: elf header corrupted")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func funtest(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func main() {
+	//	errtest()
+	//	test(12, 13)
+	//	fmt.Println("test")
+	//	fmt.Println(a, b)
+	//	strtest(b)
+
+	a := funtest(15, 17)
+	fmt.Println(a)
+	 for _, v := range map{
+        fmt.Println("map's val:", v)
+    }
+
+}
